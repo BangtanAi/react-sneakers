be7577e85b4cdd3c8f0f0508aaba8d9f2d77efe2
BangtanAi <kairova-94@mail.ru>
2021-06-24 15:49:27 +0600

    2. Урок №2 верстка корзины и основной страницы

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 function App() {
   return (
     <div className="wrapper">
+      <div className="overlay">
+        <div className="drawer"> 
+        <h2>Корзина <img className="removeBtn" src="./img/btn-remove.svg" alt="removeBtn" /></h2>        
+          <div className="items">
+            <div className="cartItem">
+              <img width={70} height={70} src="./img/1.png" alt="sneakersPhoto" />
+              <div className="cartInfo">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="./img/btn-remove.svg" alt="removeBtn" />
+            </div>
+            <div className="cartItem">
+              <img width={70} height={70} src="./img/2.png" alt="sneakersPhoto" />
+              <div className="cartInfo">
+                <p>Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              <img className="removeBtn" src="./img/btn-remove.svg" alt="removeBtn" />
+            </div>
+          </div>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого: </span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%: </span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="greenButton">Оформить заказ <img src="./img/arrow.svg" alt="Arrow" /></button>
+          </div>
+        </div>
+      </div>
       <header>
         <div className="headerLeft">
           <img width={40} height={40} src="./img/logoo.png" alt="logo" />
@@ -20,9 +58,19 @@ function App() {
         </ul>
       </header>
       <div className="content">
-        <h1>Все кроссовки</h1>
+        <div className="content-header">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="./img/search.svg" alt="search" />
+            <input placeholder="Поиск..." />
+          </div>
+        </div>
+
         <div className="sneakers">
           <div className="card">
+            <div className="favourite">
+              <img src="./img/heart-unliked.svg" alt="unlike" />
+            </div>
             <img width={133} height={112} src="./img/1.png" alt="" />
             <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
             <div className="cardInfo">
